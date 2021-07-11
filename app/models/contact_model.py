f11730529ec349e70d8ee44e284ce8c482830461
michal <michal.wilk0@yahoo.com>
2021-07-12 00:48:51 +0200

    fixed pydantic models

--- app/models/contact_model.py
+++ app/models/contact_model.py
@@ -1,20 +1,18 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import Optional
 
 
 class InviteModel(BaseModel):
-    """
-    New contact invite model
-    """
-
     my_login: str
-    signature: Optional[str]
+    signature: str
     contact_login: str
     public_id_key: str
     public_ephemeral_key: str
     otk_index: int
 
 
-class PastInvites(BaseModel):
-    number_of_invites: int
-    invites: List[InviteModel]
+class InviteModelDBO(BaseModel):
+    login: str
+    public_id_key: str
+    public_ephemeral_key: str
+    otk_index: int
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -6,18 +6,14 @@ class UserData(BaseModel):
     login: str
     public_id_key: str
     public_signed_pre_key: str
-    signature: Optional[str]
-    number_of_otk: int
-    one_time_keys: Optional[List[str]]
-
-
-class BasicUserAuthorization(BaseModel):
-    login: str
     signature: str
+    one_time_keys: List[Optional[str]]
+
+    class Config:
+        orm_mode = True
 
 
 class UserDataDTO(BaseModel):
     login: str
     public_id_key: str
     public_signed_pre_key: str
-    number_of_otk: int
